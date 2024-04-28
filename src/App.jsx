765be9f1eade2a1ca7e123a748118ca0f0656e94
Shcoobz <a.boeckmann23@gmail.com>
2024-04-28 18:09:32 +0200

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Projects from './components/pages/Projects';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <Home />
       <About />
       <Projects />
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav id='nav'>
+      <a href='#home'>HOME</a>
+      <a href='#about'>ABOUT</a>
+      <a href='#projects'>PROJECTS</a>
+      <a href='#contact'>CONTACT</a>
+    </nav>
+  );
+}
+
+export default Navbar;
